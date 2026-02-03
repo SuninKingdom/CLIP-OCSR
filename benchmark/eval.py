57f0a789da320a33dfed56692a5c6c8f41ae3006
Qixing Huang <37723943+SuninKingdom@users.noreply.github.com>
2026-02-03 16:52:36 +0800

    Create eval.py

---
+++ benchmark/eval.py
@@ -0,0 +1,32 @@
+from rdkit import Chem
+from rdkit import DataStructs
+from rdkit.Chem import AllChem
+
+
+def calculate_acc(mol_pred, mol_ref):
+    # Generate Inchi
+    ide = 0
+    if mol_pred is None:
+        return ide
+    try:
+        inchi1 = Chem.inchi.MolToInchi(mol_pred)
+        inchi2 = Chem.inchi.MolToInchi(mol_ref)
+        if inchi1 is not None and inchi2 is not None:
+            if inchi1 == inchi2:
+                ide = 1
+    except Exception as e:
+        print(f"Error processing molecule: {e}. Skipping this entry.")
+        return ide
+    return ide
+
+
+def calculate_tanimoto_similarity(mol_pred, mol_ref):
+
+    if mol_pred is not None and mol_ref is not None:
+        fp1 = AllChem.GetMorganFingerprint(mol_pred, 2)  # Morgan fingerprint with radius 2
+        fp2 = AllChem.GetMorganFingerprint(mol_ref, 2)
+
+        tanimoto_similarity = DataStructs.TanimotoSimilarity(fp1, fp2)
+        return tanimoto_similarity
+    else:
+        return 0
